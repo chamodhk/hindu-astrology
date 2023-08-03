e425969a5cca56692544827936d70418a1f76e16
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2023-08-03 23:40:47 +0530

    Update ayanamsa.py

--- ayanamsa.py
+++ ayanamsa.py
@@ -5,13 +5,13 @@ year = int(input("Enter the year: "))
 
 def calculate(year):
   ayanamsa = (year - 397)*151/3
-  minutes = int(ayanamsa/3600)
-  seconds = int((ayanamsa - (minutes*3600))/60)
-  milli_seconds = int(ayanamsa - (minutes*3600))%60
+  degrees = int(ayanamsa/3600)
+  minutes = int((ayanamsa - (minutes*3600))/60)
+  seconds = int(ayanamsa - (minutes*3600))%60
   
-  return minutes,seconds, milli_seconds
+  return degrees,minutes, seconds
 
 
-min, sec, mil = calculate(year)
+degrees,minutes, seconds = calculate(year)
 
 print(f"{min} * {sec}' {mil}''")
